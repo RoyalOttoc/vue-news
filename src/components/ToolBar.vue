b72cec9937af4053f23a993592288b00ee51ae6c
RoyalOttoc <jong.kim8706@gmail.com>
2021-05-18 05:30:03 +1000

    add router

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+    <router-link class="link" to="/news">News</router-link> |
+    <router-link class="link" to="/ask">Ask</router-link> |
+    <router-link class="link" to="/jobs">Jobs</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.header {
+  color: white;
+  background-color: #42b883;
+  display: flex;
+  padding: 8px;
+}
+
+.header .router-link-active {
+  color: #35495e;
+}
+.header a {
+  color: white;
+}
+.link {
+  padding-right: 5px;
+  padding-left: 5px;
+}
+</style>
